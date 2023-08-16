29d49fb3c0835974565f6aec9752001ca3dce8ce
MUYU_Twilighter <72800781+MUYUTwilighter@users.noreply.github.com>
2023-08-16 10:25:47 +0800

    Delete Dubbo-Interface directory

--- Dubbo-Interface/src/main/java/com/example/dubbointerface/ProviderCoupon.java
+++
@@ -1,7 +0,0 @@
-package com.example.dubbointerface;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface ProviderCoupon {
-    String distributeCoupons(String hobby) throws JsonProcessingException;
-}

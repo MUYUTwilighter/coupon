c0aa3f456d53cabee3f8efe0fa6b3825bb7ff9ff
neilzsj <1959017254@qq.com>
2023-08-16 10:46:26 +0800

    1

--- Dubbo-provider/src/main/java/com/example/dubboprovider/exercise/service/CouponDistributionService.java
+++ Dubbo-provider/src/main/java/com/example/dubboprovider/exercise/service/CouponDistributionService.java
@@ -1,8 +1,8 @@
-//package com.example.dubboprovider.exercise.service;
-//
-////import com.exercise.mapper.CouponMapper;
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//
-//public interface CouponDistributionService {
-//    String distributeCoupons(String hobby) throws JsonProcessingException;
-//}
+package com.example.dubboprovider.exercise.service;
+
+//import com.exercise.mapper.CouponMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public interface CouponDistributionService {
+    String distributeCoupons(String hobby) throws JsonProcessingException;
+}
